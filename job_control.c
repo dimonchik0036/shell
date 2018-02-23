b24eaa716792cc4de3208007a3bed41b0237f1ae
Dimonchik0036 <dimonchik0036@gmail.com>
2018-02-23 18:01:59 +0700

    [ ADD ] Add job control

--- job_control.c
+++ job_control.c
@@ -1,1 +1,28 @@
+/*
+ * Copyright © 2018 Dimonchik0036. All rights reserved.
+ */
+
+
 #include "job_control.h"
+
+
+JobController *job_controller_create() {
+    JobController *controller = malloc(sizeof(JobController));
+    if (!controller) {
+        fprintf(stderr, "Couldn't allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    job_controller_init(controller);
+    return controller;
+}
+
+void job_controller_free(JobController *controller) {
+    free(controller); //TODO add memory cleanup for Job[]
+}
+
+void job_controller_init(JobController *controller) {
+    memset(controller->jobs, 0, sizeof(Job *) * JOB_LIMIT);
+    controller->current_max_jid = 1;
+    controller->number_of_jobs = 0;
+}
--- job_control.h
+++ job_control.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2018 Dimonchik0036. All rights reserved.
+ */
+
+
 #ifndef JOB_CONTROL_H
 #define JOB_CONTROL_H
 
@@ -5,17 +10,42 @@
 #include "command.h"
 
 
+#define JOB_LIMIT 32
+
+
+#define STOPPED 1
+#define RUNNING 2
+#define DONE 4
+#define FAILED 8
+
+
+typedef int jid_t;
+
 struct Job_St {
-    int jid;
+    jid_t jid;
     pid_t pid;
     Command *command;
-    char flag;
-    char *infile;
-    char *outfile;
-    char appfile;
+    char status;
 };
 
 typedef struct Job_St Job;
+
+struct JobController_St {
+    Job *jobs[JOB_LIMIT];
+    jid_t current_max_jid;
+    int number_of_jobs;
+};
+
+typedef struct JobController_St JobController;
+
+
+JobController *job_controller_create();
+
+void job_controller_free(JobController *controller);
+
+void job_controller_init(JobController *controller);
+
+void job_controller_add_job(pid_t pid, Command *command);
 
 
 #endif //JOB_CONTROL_H
